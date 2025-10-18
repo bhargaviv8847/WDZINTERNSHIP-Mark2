2c0a10db6d8423a3044bf5ed509c3adc69e724ae
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-18 20:44:02 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
-    path('', views.home, name='home'),  # Add this for the homepage
 ]
