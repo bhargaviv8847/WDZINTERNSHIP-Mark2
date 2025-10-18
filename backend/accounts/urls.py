8752372a31f613d3628d4e74dcd7e24500a5f998
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-18 21:09:58 +0530

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,11 @@
-from django.contrib import admin
-from django.urls import path
-from accounts import views
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
+
+    # Catch-all for React SPA
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
