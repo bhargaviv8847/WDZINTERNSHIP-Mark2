864849a80c208b512523014b9cac165f374e4987
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-18 20:36:48 +0530

    Add homepage URL path to urlpatterns

--- backend/urls.py
+++ backend/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
+    path('', views.home, name='home'),  # Add this for the homepage
 ]
