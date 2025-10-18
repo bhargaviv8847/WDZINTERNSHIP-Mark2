65df1b299c7c549a3616b6ad3a95c01fb014bcd2
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-18 20:59:27 +0530

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,7 +4,6 @@ from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.welcome, name='home'),  # 👈 This makes "/" the home page
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
 ]
