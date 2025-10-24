d4b6661fb773ac33fea9c8f39e7de5452864ae20
GANESH-08-CODER <chsivasatyasaiganesh@gmail.com>
2025-10-24 19:18:52 +0530

    Add files via upload

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import accounts
+
+class accountsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=accounts
+        fields='__all__'
