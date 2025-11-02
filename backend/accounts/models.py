d5d10a459bd970993980e80331c289dc05a59bc6
GANESH-08-CODER <chsivasatyasaiganesh@gmail.com>
2025-11-02 20:06:49 +0530

    Update models.py

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,15 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
 class User(AbstractUser):
     def __str__(self):
         return self.username
-
-class accounts(models.Model):
+class Account(models.Model):   
     title = models.CharField(max_length=200)
     description = models.TextField()
     instructor = models.CharField(max_length=100)
     duration = models.PositiveIntegerField(help_text="Duration in hours")
 
-    def _str_(self):
+    def __str__(self):
+        return self.title
+
+def upload_video_path(instance, filename):
+    return f'videos/{instance.title}/{filename}'
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    video_file = models.FileField(upload_to=upload_video_path)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
         return self.title
