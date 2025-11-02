9a762bbbcf9e539f2863061408c2922b8f9aafa0
GANESH-08-CODER <chsivasatyasaiganesh@gmail.com>
2025-11-02 20:13:19 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from accounts import views
+from accounts import views  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
+
+    path('upload/', views.VideoUploadView.as_view(), name='video-upload'),
+    path('videos/', views.VideoListView.as_view(), name='video-list'),
 ]
